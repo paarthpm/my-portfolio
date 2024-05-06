2fdea7b15ec0453ed2faea77f726178edfdb129d
Partha Pratim <partha0120@gmail.com>
2024-05-06 09:46:20 +0530

    Site Look Demo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Navigation from "./components/Navigation"
+function App() {
+  return (
+    <main>
+      <Header/>
+      <Navigation/>
+    </main>
+  )
+}
+
+export default App
