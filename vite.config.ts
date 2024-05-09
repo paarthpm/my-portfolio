1c91784dd636d07cd4df207df02784032fbeb31b
paarthpm <partha0120@gmail.com>
2024-05-09 12:47:49 +0530

    vite config base renamed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const repositoryName = 'my-potfolio';
+const repositoryName = 'my-portfolio';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: `/${repositoryName}/`,
